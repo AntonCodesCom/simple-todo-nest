210aeddc181125f16ce3c5c846857818f2369faf
Anton <antoncodes@outlook.com>
2024-05-14 15:42:50 +0300

    todo fixture initializer

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,7 +1,25 @@
+import { faker } from '@faker-js/faker';
 import { Todo as TodoModel } from '@prisma/client';
+import { aliceUserId } from '../fixtures/user-ids';
 
+/**
+ * Todo entity.
+ */
 export class TodoEntity implements TodoModel {
   id: string;
   label: string;
   userId: string;
+}
+
+function initTodo(partial: Partial<TodoEntity>): TodoEntity {
+  return {
+    id: partial.id || faker.string.uuid(),
+    label: partial.label ?? faker.lorem.sentence(),
+    userId: partial.userId || aliceUserId,
+  };
+}
+
+// for data seeding
+export function initTodos(partials: Partial<TodoEntity>[]): TodoEntity[] {
+  return partials.map(initTodo);
 }
--- src/todo/fixtures/todos.ts
+++ src/todo/fixtures/todos.ts
@@ -1,29 +1,23 @@
-import { TodoEntity } from '../entities/todo.entity';
+import { TodoEntity, initTodos } from '../entities/todo.entity';
+import { aliceUserId, bobUserId } from './user-ids';
 
-const aliceUserId = '878664be-1926-44ab-9c77-eb5d803369be';
-const bobUserId = '5db73b24-bbc4-49a3-b6a5-ec36a240a7d5';
-
-const todos: TodoEntity[] = [
+const todos: TodoEntity[] = initTodos([
   {
-    id: '1',
     label: 'Initialize Remix frontend.',
     userId: aliceUserId,
   },
   {
-    id: '2',
     label: 'Setup NestJS backend.',
     userId: aliceUserId,
   },
   {
-    id: '3',
     label: 'Integrate frontend with backend.',
     userId: aliceUserId,
   },
   {
-    id: '4',
     label: 'Learn React & Node.js!',
     userId: bobUserId,
   },
-];
+]);
 
 export default todos;
