0dc9fb22db05bc313ffd29f5cd5c51172ce1450e
Anton <antoncodes@outlook.com>
2024-05-17 23:16:23 +0300

    done state for fixtures

--- src/todo/fixtures/todos.ts
+++ src/todo/fixtures/todos.ts
@@ -6,19 +6,22 @@ const todos: TodoEntity[] = initTodos([
     id: 'ef1e68db-6e1d-42f0-8e31-574694236062',
     userId: aliceUserId,
     label: 'Initialize Remix frontend.',
-    done: false,
+    done: true,
   },
   {
     userId: aliceUserId,
     label: 'Setup NestJS backend.',
+    done: true,
   },
   {
     userId: aliceUserId,
     label: 'Integrate frontend with backend.',
+    done: false,
   },
   {
     userId: bobUserId,
     label: 'Learn React & Node.js!',
+    done: false,
   },
 ]);
 
