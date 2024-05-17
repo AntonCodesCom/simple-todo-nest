b0030232611e07a62d9dc71828da272371a5af10
Anton <antoncodes@outlook.com>
2024-05-17 23:31:45 +0300

    createdAt for fixtures

--- src/todo/fixtures/todos.ts
+++ src/todo/fixtures/todos.ts
@@ -6,14 +6,17 @@ const todos: TodoEntity[] = initTodos([
     id: 'ef1e68db-6e1d-42f0-8e31-574694236062',
     label: 'Initialize Remix frontend.',
     userId: aliceUserId,
+    createdAt: new Date('2024-05-17T20:00:00.000Z'),
   },
   {
     label: 'Setup NestJS backend.',
     userId: aliceUserId,
+    createdAt: new Date('2024-05-17T20:10:00.000Z'),
   },
   {
     label: 'Integrate frontend with backend.',
     userId: aliceUserId,
+    createdAt: new Date('2024-05-17T20:20:00.000Z'),
   },
   {
     label: 'Learn React & Node.js!',
