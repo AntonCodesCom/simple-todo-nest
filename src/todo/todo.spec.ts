4c1dd17ec53dc41d76571f0ac3eb7707a82a1124
Anton <antoncodes@outlook.com>
2024-06-04 19:35:16 +0300

    removing e2e validation tests

--- src/todo/todo.spec.ts
+++ src/todo/todo.spec.ts
@@ -1,11 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { TodoService } from './todo.service';
 import { TodoModule } from './todo.module';
 import { CreateTodoDto } from './dto/create-todo.dto';
-import { validate } from 'class-validator';
-import validatorOptions from 'src/common/validator-options';
 import { aliceUserId } from './fixtures/user-ids';
 import getRandomObjectArray from 'src/common/utils/getRandomObjectArray';
 import { faker } from '@faker-js/faker';
@@ -39,7 +37,6 @@ describe('Todo REST', () => {
       .useValue(mockTodoService)
       .compile(); // TODO: override interceptor (AuthInterceptor)
     app = moduleFixture.createNestApplication();
-    app.useGlobalPipes(new ValidationPipe(validatorOptions));
     await app.init();
   });
 
@@ -79,22 +76,6 @@ describe('Todo REST', () => {
 
     test('invalid authorization', async () => {
       await request(app.getHttpServer()).post('/todo').expect(401);
-    });
-
-    test('invalid request body', async () => {
-      // Defining body that will always be invalid against the target `CreateTodoDto`
-      // (just don't define `CreateTodoDto.invalidProperty` within the DTO itself).
-      // This approach will, however, work only when unknown body properties are
-      // supposed to be rejected by endpoints.
-      const invalidBody = new CreateTodoDto();
-      (invalidBody as any).invalidProperty = true;
-      const validationErrors = await validate(invalidBody, validatorOptions);
-      expect(validationErrors.length).toBeGreaterThan(0);
-      await request(app.getHttpServer())
-        .post('/todo')
-        .set('Authorization', authorizationHeader)
-        .send(invalidBody)
-        .expect(400);
     });
   });
 
@@ -144,18 +125,6 @@ describe('Todo REST', () => {
         .patch(`/todo/${invalidTodoId}`)
         .set('Authorization', authorizationHeader)
         .send(validBody)
-        .expect(400);
-    });
-
-    test('invalid request body', async () => {
-      const invalidBody = new UpdateTodoDto();
-      (invalidBody as any).invalidProperty = true;
-      const validationErrors = await validate(invalidBody, validatorOptions);
-      expect(validationErrors.length).toBeGreaterThan(0);
-      await request(app.getHttpServer())
-        .patch(`/todo/${mockTodoId}`)
-        .set('Authorization', authorizationHeader)
-        .send(invalidBody)
         .expect(400);
     });
   });
