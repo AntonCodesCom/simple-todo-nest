d201f2653ba684003bb97350e89584561ec82e1d
Anton <antoncodes@outlook.com>
2024-06-20 17:42:21 +0300

    auht module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { EnvModule } from './env/env.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TodoModule } from './todo/todo.module';
 import { SeedModule } from './seed/seed.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [EnvModule, PrismaModule, TodoModule, SeedModule],
+  imports: [AuthModule, EnvModule, PrismaModule, TodoModule, SeedModule],
   controllers: [AppController],
 })
 export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('me')
+  me() {}
+
+  @Post('login')
+  login() {}
+}
