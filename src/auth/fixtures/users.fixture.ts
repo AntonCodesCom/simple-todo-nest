0c218973dc49b778331a7c4251ea51bb518ee7f0
Anton <antoncodes@outlook.com>
2024-06-21 14:32:44 +0300

    doc: users fixture requirements

--- src/auth/fixtures/users.fixture.ts
+++ src/auth/fixtures/users.fixture.ts
@@ -1,15 +1,21 @@
 import { UserEntity } from '../entities/user.entity';
 
+/**
+ * Users fixture.
+ *
+ * This fixture MUST contain a user with username `alice` and password `Alice1111$`
+ * in order for end-to-end tests to function correctly.
+ */
 const usersFixture: UserEntity[] = [
   {
     id: '878664be-1926-44ab-9c77-eb5d803369be',
     username: 'alice',
-    passwordHash: 'User1111$', // TODO: argon2
+    passwordHash: 'Alice1111$', // TODO: argon2
   },
   {
     id: '5db73b24-bbc4-49a3-b6a5-ec36a240a7d5',
     username: 'bob',
-    passwordHash: 'User2222$', // TODO: argon2
+    passwordHash: 'Bob2222$', // TODO: argon2
   },
 ];
 
